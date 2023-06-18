df12453d5fa949e41ae0fbc194f1547ba834b878
Tasha Kibanova <ov.nattie@gmail.com>
2023-06-18 14:13:42 +0500

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = Math.pow(b, 2) - 4*a*c;
+  if (discriminant < 0) {
+    arr == 0;
+  } else if (discriminant == 0) {
+    arr = -b / (2*a);
+  } else if (discriminant > 0) {
+    arr.push((-b + Math.sqrt(discriminant) )/(2*a));
+    arr.push((-b - Math.sqrt(discriminant) )/(2*a));
+  }
   return arr;
 }
 
